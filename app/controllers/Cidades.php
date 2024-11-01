47384b39d2e3a20ffa770670692c79ad99211dae
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-11-01 00:40:24 +0000

    Nova Cidades

--- app/controllers/Cidades.php
+++ app/controllers/Cidades.php
@@ -4,9 +4,10 @@ Class Cidades extends Controller {
     public function getCidades($id_uf) {
         // Inicializa o modelo "Cidades"
         $cidades = $this->model('Cidades');
+        $cidades->uf = $id_uf;
 
         // Call function from the model
-        $cidadesData = $cidades->getCidades($id_uf);
+        $cidadesData = $cidades->getCidades();
 
         return $cidadesData;
     }
