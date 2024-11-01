4041d217e9bfa915fe4691ad6540cb5134570e8d
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-11-01 00:39:53 +0000

    Nova Cidades

--- app/models/Cidades.php
+++ app/models/Cidades.php
@@ -4,11 +4,11 @@ class Cidades {
     public $id;
     public $uf;
 
-    public function getCidades($id_uf) {
+    public function getCidades() {
         $sqlQuery = "SELECT * FROM cidades WHERE uf = ?";
 
         try {
-            return Database::query($sqlQuery, [$id_uf]);
+            return Database::query($sqlQuery, [$this->uf]);
         } catch (\PDOException $e) {
             exit($e->getMessage());
         }
