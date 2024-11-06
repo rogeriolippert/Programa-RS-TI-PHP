5c3e9d31d2aef712f05453deb5fbbdf31c0164fc
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-11-06 00:22:04 +0000

    Save

---
+++ app/controllers/API.php
@@ -0,0 +1,17 @@
+<?php
+
+Class API extends Controller {
+    // Exemplo de URL de chamada no nosso JavaScript
+    // Retorna todas as cidades do estado do RS:
+    // /public/index.php?url=API/JSON/Cidades/getCidades/23
+
+    // API => Controller
+    // JSON => Método
+    // Cidades => Parâmetro 1
+    // getCidades => Parâmetro 2
+    // 23 => Parâmetro 3 ($id_uf)
+
+    public function JSON($controller, $method, $params = null) {
+        die(var_dump($controller, $method, $params));
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,5 +4,3 @@ require_once '../app/init.php';
 
 // Create a new instance of our app when someone hits the index.php
 $app = new App;
-
-?>
